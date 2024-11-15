c3155db300a32b39f777443c349c58351fe36a29
luismeza8 <angel841bv@gmail.com>
2024-11-14 21:06:42 -0700

    borrar habito

--- habits/urls.py
+++ habits/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('crear_habito/', views.crear_habito, name='crear_habito'),
+    path('borrar_habito/<int:pk>', views.borrar_habito, name='borrar_habito'),
+    path('editar_habito/<int:pk>', views.editar_habito, name='editar_habito'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -21,3 +21,13 @@ def crear_habito(request):
             return redirect('home')
 
     return render(request, 'habits/home.html')
+
+
+def borrar_habito(request, pk):
+    habito = Habito.objects.get(pk=pk)
+    habito.delete()
+    return redirect('home')
+
+
+def editar_habito(request, pk):
+    pass
