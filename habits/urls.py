83d9c3efffe2bb4a6934b73ceaf8e8768145b2b4
ChiefBadass <carlosdrt900@gmail.com>
2024-11-29 03:07:07 -0600

    se agrego grafica del proceso de habitos

--- habits/urls.py
+++ habits/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('borrar_habito/<int:pk>', views.borrar_habito, name='borrar_habito'),
     path('editar_habito/<int:pk>', views.editar_habito, name='editar_habito'),
     path('alternar_dia/<int:pk>/', views.alternar_dia, name='alternar_dia'),
+    path('habit/<int:habit_id>/graph/', views.grafica_habito, name='grafica_habito'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -4,6 +4,7 @@ from datetime import date, datetime, timedelta
 from habits.forms import HabitoForm
 from habits.models import Habito
 from habits.models import Dia
+import json
 
 def generar_dias(habito):
     # Define el rango de fechas (del 10/11 al 16/11)
@@ -68,3 +69,20 @@ def alternar_dia(_, pk):
     dia.save()
     
     return redirect('home') 
+
+def grafica_habito(request, habit_id):
+    habit = get_object_or_404(Habito, id=habit_id)
+    
+    actividades = habit.obtener_ultimos_7_dias()
+
+    realizado_count = sum(1 for actividad in actividades if actividad.realizado)
+    no_realizado_count = len(actividades) - realizado_count
+
+    realizado_json = json.dumps(realizado_count)
+    no_realizado_json = json.dumps(no_realizado_count)
+
+    return render(request, 'habits/grafica_habito.html', {
+        'habit_id': habit_id,
+        'realizado_json': realizado_json,
+        'no_realizado_json': no_realizado_json,
+    })
